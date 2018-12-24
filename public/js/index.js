4c12a86b63bb8a64e09b2dbd57a545400adec04d
Nishant <vnishant101297@gmail.com>
2018-12-25 02:13:24 +0530

    Add css

--- public/js/index.js
+++ public/js/index.js
@@ -43,24 +43,28 @@ socket.on('newLocationMessage',function(message){
 });
 jQuery('#message-form').on('submit',function(e){
   e.preventDefault();
+  var messageTextBox=jQuery('[name=message]');
   socket.emit('createMessage',{
     from:'User',
     text:jQuery('[name=message]').val()
   },function(){
-
+jQuery('[name=message]').val('')
   });
 });
 var locationButton=jQuery('#send-location');
 locationButton.on('click',function(){
   if(!navigator.geolocation)
   {return alert('Geolocation not supported by your browser');}
+locationButton.attr('disabled','disabled').text('sending location...');
   navigator.geolocation.getCurrentPosition(function(position){
     // console.log(position);
+    locationButton.removeAttr('disabled').text('Send Location');
     socket.emit('createLocationMessage',{
       latitude:position.coords.latitude,
       longitude:position.coords.longitude
     });
   },function(){
+    locationButton.removeAttr('disabled').text('Send Location');
     alert('Unable to fetch location');
   });
 });
